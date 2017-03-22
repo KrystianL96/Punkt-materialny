99e195322c445fd2e81b9bf2b19ce008319bf748
KrystianL96 <krystian22.05@interia.pl>
2017-03-22 17:44:22 +0100

    Labolatorium_1: Punkt materialny

--- Projekt_Pkt_Materialny/src/PktMat.java
+++ Projekt_Pkt_Materialny/src/PktMat.java
@@ -1,4 +1,4 @@
-public class PktMat {
+public class PktMat { //klasa reprezentuj¹ca pkt. materialny
 private double m;
 private double d;
 public PktMat() { //konstruktor domyœlny
@@ -7,15 +7,17 @@ public PktMat() { //konstruktor domyœlny
 public PktMat(double masa )	{ //konstruktor z parametrem
 	this.m = masa;
 }
-public double zmianaMasy(double a) { //akcesor kontroli wartoœci
+public void setMasa(double a) { //akcesor typu set
 	this.m=a;
-	return this.m;
+}
+public double getMasa() { //akcesor typu get
+	return m;
 }
 public void opis() { //podanie informacji o punkcie materialnym
 	System.out.println("Punkt materialny");
 	System.out.format("Masa: %f%n",m);
-	System.out.format("G³owny moment bezw³adnoœci: %f%n",moment());
-	System.out.format("Moment bezw³adnoœci wzglêdem zmienionej osi: %f%n",steiner(d));
+	System.out.format("G³ówny moment bezw³adnoœci: %f%n",moment());
+	System.out.format("Moment bezw³adnoœci wzglêdem zmienionej osi: %f\n\n",steiner(d));
 }
 public double moment () { //obliczenie g³ównego momentu bezw³adnoœci = 0
 	return 0;
--- Projekt_Pkt_Materialny/src/PunktMat.java
+++ Projekt_Pkt_Materialny/src/PunktMat.java
@@ -1,21 +1,21 @@
 
-public class PunktMat extends PktMat{
+public class PunktMat extends PktMat{ //klasa testowa
 
 	public static void main(String[] args) {
-		PktMat pkt1 = new PktMat();
-		PktMat pkt2 = new PktMat(20);
-		pkt1.steiner(5);
+		PktMat pkt1 = new PktMat();	//tworzenie obiektu przez konstruktor domyœlny
+		PktMat pkt2 = new PktMat(20);	//tworzenie obiektu przez konstruktor z parametrem
+		pkt1.steiner(5);	//obliczenie nowego momentu bezw³adnoœci
 		pkt2.steiner(5);
-		pkt1.opis();
+		pkt1.opis();	//wyœwietlenie opisu pkt. materialnego
 		pkt2.opis();
-		pkt1.zmianaMasy(2);
+		pkt1.setMasa(2);	//ustawienie nowej masy
 		pkt1.opis();
-		PktMat[] tablica = new PktMat[5];
+		PktMat[] tablica = new PktMat[5];	//utworzenie tablicy obiektów
 		for (int i=0; i<5; i++){
-			PktMat pkt = new PktMat(i+1);
-			tablica[i] = pkt;
+			PktMat pkt = new PktMat(i+1);	//utworzenie punktów dla tablicy obiektów
+			tablica[i] = pkt;				//przypisanie punktów do tablicy
 		}
-		for(PktMat punkt : tablica){
+		for(PktMat punkt : tablica){	//wyœwietlenie opisu dla pkt. z tablicy obiektów
 			punkt.steiner(5);
 			punkt.opis(); 
 		}
